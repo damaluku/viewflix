e967af2678cade4956696d223e67521aad84251e
damaluku <damaluku@gmail.com>
2023-08-03 01:59:26 +0100

    meta tags configured

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
 import AuthLayout from "@/layouts/AuthLayout";
+import Head from "next/head";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -22,15 +23,53 @@ export default function App({
 
   if (Component.getLayout) {
     return Component.getLayout(
-      <AuthLayout>
-        <Component {...pageProps} />
-      </AuthLayout>
+      <>
+        <Head>
+          <title>Viewflix</title>
+          <meta name="title" content="Viewflix" />
+          <meta
+            name="description"
+            content="A beautiful and advanced Netflix clone."
+          />
+          <meta
+            name="keywords"
+            content="Youtube, magic, Netflix, movies, React, Nextjs, music, series"
+          />
+          <meta name="robots" content="index, follow" />
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
+          <meta name="revisit-after" content="15 days" />
+          <meta name="author" content="Damian Aluku" />
+        </Head>
+        <AuthLayout>
+          <Component {...pageProps} />
+        </AuthLayout>
+      </>
     );
   }
 
   return (
-    <BaseLayout>
-      <Component {...pageProps} />
-    </BaseLayout>
+    <>
+      <Head>
+        <title>Viewflix</title>
+        <meta name="title" content="Viewflix" />
+        <meta
+          name="description"
+          content="A beautiful and advanced Netflix clone."
+        />
+        <meta
+          name="keywords"
+          content="Youtube, magic, Netflix, movies, React, Nextjs, music, series"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="15 days" />
+        <meta name="author" content="Damian Aluku" />
+      </Head>
+      <BaseLayout>
+        <Component {...pageProps} />
+      </BaseLayout>
+    </>
   );
 }
